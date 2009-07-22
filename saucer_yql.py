673df741b331dee8ba16d1f4b598cd949dda15c9
Luke Lee <durdenmisc@gmail.com>
2009-07-21 20:05:08 -0500

    Cleaned up saucer.py file to use appengine Saucer class and be able to create csv files for manual importing

--- saucer_yql.py
+++ saucer_yql.py
@@ -3,110 +3,100 @@
 import urllib
 import simplejson
 import re
+import sys
 
-# FIXME: Consistent variable names, casing, etc.
-# FIXME: Break up strings better
+from api.saucer import Saucer
 
 class Beer():
     def __init__(self, name, type, style, descr):
-       self.name = self.__sanitize__(name)
-       self.type = self.__sanitize__(type)
-       self.style = self.__sanitize__(style)
-       self.descr = self.__sanitize__(descr)
+       self.name = name
+       self.type = type
+       self.style = style
+       self.descr = descr
 
-    def __str__(self):
-        return "%s (%s):\n    %s\n    %s" % (self.name, self.type,
+    # Really only used for printing for csv output
+    def csv(self):
+        # Subs for uploading to app engine with uploader class
+        self.name = re.sub('"', '""', self.name)
+        self.type = re.sub('"', '""', self.type)
+        self.style = re.sub('"', '""', self.style)
+        self.descr = re.sub('"', '""', self.descr)
+        return "\"%s\",\"%s\",\"%s\",\"%s\"\n" % (self.name, self.type,
                                             self.style, self.descr)
 
-    def __sanitize__(self, arg):
-        # Verify the object is a string, otherwise default it
-        if (not isinstance(arg, str)):
-            return "N/A"
-
-        # Suppress multiple spaces and newlines
-        return re.sub(' +', ' ', re.sub('\n', '', arg)).strip()
+    def __str__(self):
+        return "Name:%s\nType:%s\nStyle:%s\nDescription:%s\n\n" % (self.name, self.type,
+                                            self.style, self.descr)
  
-class SaucerApi():
-    BOTTLE = "Bottle"
-    DRAFT = "Draft"
-
-    __btl_str__ = r"\(BTL\)"
-
-    def __fetch_json__(self, url):
-        base_url = "http://query.yahooapis.com/v1/public/yql"
-        return simplejson.load(urllib.urlopen( "%s?%s" % (base_url, url)))
-
-    def getAllBeers(self):
-        url = urllib.urlencode({"format":"json",
-            "q":"select * from html where url=\"http://www.beerknurd.com/store.sub.php?store=6&sub=beer&groupby=name\" and xpath='//select[@id=\"brews\"]/option'"})
-
-        res = self.__fetch_json__(url)
-
-        # Hide the ugly yql/html parsing and create list of dictionaries 
-        beers = []
-        for tmp in res['query']['results']['option']:
-            beer = {}
-
-            beer['name'] = tmp['content'].strip()
-            beer['id'] = tmp['value'].strip()
-            beer['type'] = SaucerApi.DRAFT 
-
-            # Bottle or draft?
-            if re.compile(SaucerApi.__btl_str__).search(beer['name']):
-                beer['type'] = SaucerApi.BOTTLE
-
-                # Remove the bottle string in name
-                beer['name'] = re.sub(SaucerApi.__btl_str__, '', beer['name'])
-
-            beers.append(beer)
-
-        return beers
-
-    def getBeerDetails(self, beer):
-        xpath= "xpath='//table/tr/td/p'"
-        url = urllib.urlencode({"format":"json",
-            "q":"select * from html where url=\"http://www.beerknurd.com/store.beers.process.php?brew=%s\" and %s" % (beer, xpath)})
-
-        res = self.__fetch_json__(url)
-
-        # Create a dictionary b/c it's easier to work with
-        # FIXME: Sanitize the keys in dictionary
-        return dict(zip(res['query']['results']['p'][::2],
-                    res['query']['results']['p'][1::2]))
-
-def main():
+def main(file, max):
+    saucer = Saucer()
     bottles = []
     drafts = []
-    saucer = SaucerApi()
+    ids = []
     ii = 0
+    group = 20
+
+    if max < group:
+        group = max
 
     beers = saucer.getAllBeers()
 
     for beer in beers:
-        details = saucer.getBeerDetails(beer['id'])
-        brew = Beer(beer['name'], beer['type'], details['Style:'],
-                    details['Description:'])
+        ids.append(beer['id'])
+        ii += 1
+        if ii >= group:
+            break
 
-        if beer['type'] == SaucerApi.BOTTLE:
+    details = saucer.getBeerDetails(ids)
+    ii = 0
+
+    for beer in beers:
+        brew = Beer(beer['name'], beer['type'], details[ii]['Style:'],
+                    details[ii]['Description:'])
+
+        if beer['type'] == Saucer.BOTTLE:
             bottles.append(brew)
         else:
             drafts.append(brew)
 
-        # FIXME: Just process the first 20
         ii += 1
-        if ii >= 20:
+
+        if (max > 0 and ii >= max) or ii >= len(details):
             break
 
     if len(drafts):
-        print "\n\nDrafts:"
+        if file is None:
+            print "\n\nDrafts:\n"
+
         for beer in drafts:
-            print beer
+            if file is None:
+                print beer
+            else:
+                file.write("%s" % beer.csv())
 
     if len(bottles):
-        print "\n\nBottles:"
+        if file is None:
+            print "\n\nBottles:\n"
+
         for beer in bottles:
-            print beer
+            if file is None:
+                print beer
+            else:
+                file.write("%s" % beer.csv())
 
 
 if __name__ == "__main__":
-    main()
+    file = None
+    max = 0
+
+    # FIXME: Catch errors
+    if len(sys.argv) > 1:
+        max = int(sys.argv[1])
+
+    if len(sys.argv) > 2:
+        file = open(sys.argv[2], 'w')
+
+    main(file, max)
+
+    if file is not None:
+        file.close()
